c1d6f4b775617da602b36ab1c990563aae9fa556
HetSavani36 <hetsavani36@gmail.com>
2024-03-07 20:48:01 +0530

    c++

--- binary search/pivotIndex.cpp
+++ binary search/pivotIndex.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
 
-    int a[]={3,8,10,17,1};
+    int a[]={3,8,1,4,5};
     int n=5;
     int low=0,high=n-1,mid=low+(high-low)/2;
 
---
+++ binary search/upperBound.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a[]={1,2,3,3,3,4,4,5};
+    int n=8;
+    int tar;
+    cout<<"target: ";
+    cin>>tar;
+    int ans=-1;
+
+    int low=0,high=n-1,mid=low+(high-low)/2;
+    while(low<=high){
+        if(tar==a[mid]){
+            ans=mid;
+            low=mid+1;
+        }
+        else if(a[mid]>tar) high=mid-1;
+        else low=mid+1;
+        mid=low+(high-low)/2;
+    }
+    cout<<ans;
+    
+    return 0;
+}
